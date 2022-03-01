1c2a94b91a9bc8d8d15e9011578b84bc7ee71083
shawon-MG <work.shawomg@gmail.com>
2022-03-02 03:54:19 +0600

    2nd push

--- js/api.js
+++ js/api.js
@@ -11,5 +11,24 @@ const btnClicked = () =>{
 } 
 
 const displaySearchResult = data =>{
-    console.log(data);
+    const searchResult = document.getElementById('search-result');
+    data.forEach(data => {
+        console.log(data);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="col>
+        <div class="card h-100">
+        <img src="${data.image}" class="card-img-top" alt="">
+        <div class="card-body">
+          <h6 class="card-title">Phone Name: ${data.phone_name}<h6>
+          <h6 class="card-title">Brand: ${data.brand}</h6>
+        </div>
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">                      
+            <button type="button" class="btn btn-warning rounded-pill m-4">Details</button>
+        </div>
+      </div>
+        `;
+        searchResult.appendChild(div);
+    })
 }
